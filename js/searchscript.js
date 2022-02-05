64dd2d476693f56b23f7da01950a8ac7d592a40f
HarshLulla23 <d2019.harsh.lulla@ves.ac.in>
2022-02-05 16:24:48 +0530

    seach page updated

--- js/searchscript.js
+++ js/searchscript.js
@@ -7,43 +7,66 @@ const suggBox = searchinput.querySelector(".autocom-box");
 
 inputBox.onkeyup = (e) => {
     let userData = e.target.value.toLocaleLowerCase(); //user entered data
-    let emptyArray =[];
-    if(userData){
-        emptyArray = suggestions.filter((data)=>{
-            //filtering array value and user char to lowercase and return only those words which starts with userdata
-            return data.toLocaleLowerCase().startsWith(userData);
-        });
-        // console.log(emptyArray);
-        emptyArray = emptyArray.map((data)=>{
-            return data = '<li>'+ data+'</li>';
-        });
-        // console.log(emptyArray); //remove comment to see suggestion array on console
-        searchinput.classList.add("active"); // show autocomplete box
-        showSuggestions(emptyArray);
-        let allList = suggBox.querySelectorAll("li");
-        for(let i =0; i < allList.length; i++){
-            // adding onclick attribiute to all li tag
-            allList[i].setAttribute("onclick","select(this)");
+    let emptyArray = [];
+    let suggestions1 = [];
+    const xhr = new XMLHttpRequest();
+
+    // Open an obejct (GET/POST, PATH,
+    // ASYN-TRUE/FALSE)
+    xhr.open("GET","https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords="+userData+"&apikey=RGLFHEB1DW0JDEF1", true);
+
+    // When response is ready
+    xhr.onload = function () {
+        if (this.status === 200) {
+
+            // Changing string data into JSON Object
+            var obj = JSON.parse(this.responseText);
+
+            for (key in obj["bestMatches"]) {
+                suggestions1.push(obj["bestMatches"][key]["2. name"]);
+            }
+
+            if (userData) {
+                emptyArray = suggestions1.filter((data) => {
+                    //filtering array value and user char to lowercase and return only those words which starts with userdata
+                    return data.toLocaleLowerCase();
+                });
+                emptyArray = emptyArray.map((data) => {
+                    return data = '<li>' + data + '</li>';
+                });
+                // console.log(emptyArray); //remove comment to see suggestion array on console
+                searchinput.classList.add("active"); // show autocomplete box
+                showSuggestions(emptyArray);
+                let allList = suggBox.querySelectorAll("li");
+                for (let i = 0; i < allList.length; i++) {
+                    // adding onclick attribiute to all li tag
+                    allList[i].setAttribute("onclick", "select(this)");
+                }
+            }
+            else {
+                searchinput.classList.remove("active"); //hide auto complete box
+            }
+        }
+        else {
+            console.log("File not found");
         }
     }
-    else{
-        searchinput.classList.remove("active"); //hide auto complete box
-    }
+    xhr.send();
 }
-function select(element){
+function select(element) {
     let selectUserData = element.textContent;
     inputBox.value = selectUserData; //passing the user selected list item in textfield
     searchinput.classList.remove("active"); //hide auto complete box
 
 }
 
-function showSuggestions(list){
+function showSuggestions(list) {
     let listData;
-    if(!list.length){
+    if (!list.length) {
         userValue = inputBox.value;
-        listData = '<li>'+userValue+'</li>';
+        listData = '<li>' + userValue + '</li>';
     }
-    else{
+    else {
         listData = list.join('');
     }
     suggBox.innerHTML = listData;
